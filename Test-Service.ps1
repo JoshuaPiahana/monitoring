d9be55952d09587eed83b69517f6d77773e83adb
JoshuaPiahana <nzsteiny@gmail.com>
2022-01-18 20:04:44 +1300

    Created Test-Service function

---
+++ Test-Service.ps1
@@ -0,0 +1,14 @@
+function Test-Service {
+    param (
+        $Name
+    )
+    {
+        if ((Get-Service -Name $Name | Select-Object -ExpandProperty 'Status') -eq 'Running'){
+            return $true
+        }
+
+        else {
+            return $false
+        }
+    }
+}
