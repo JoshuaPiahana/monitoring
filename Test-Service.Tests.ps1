fad3edc0af674a40faa6d712a6c76c6ece649fa7
JoshuaPiahana <nzsteiny@gmail.com>
2022-01-18 22:14:13 +1300

    Removed context block

--- Test-Service.Tests.ps1
+++ Test-Service.Tests.ps1
@@ -3,29 +3,27 @@ BeforeAll {
 }
 
 Describe "Test-Service" {
-    Context "ContextName" {
-        $ServicesRegistryPath = "TestRegistry:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services"
-        New-Item -Path "$ServicesRegistryPath\MockRunningService"
-        New-ItemProperty 
-        
-        $MockRunningService = New-MockObject -Type 'System.ServiceProcess.ServiceController' `
-        -Properties @{
-            "ServiceName" = "MockRunningService"; 
-            "Status" = "Running"
-        }
+    $ServicesRegistryPath = "TestRegistry:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services"
+    New-Item -Path "$ServicesRegistryPath\MockRunningService"
+    New-ItemProperty 
+    
+    $MockRunningService = New-MockObject -Type 'System.ServiceProcess.ServiceController' `
+    -Properties @{
+        "ServiceName" = "MockRunningService"; 
+        "Status" = "Running"
+    }
 
-        $MockStoppedService = New-MockObject -Type 'System.ServiceProcess.ServiceController' `
-        -Properties @{
-            "ServiceName" = "MockStoppedService";
-            "Status" = "Stopped"
-        }
-        
-        It "Given the name of a service with a status of 'Running', will return true" {
-            Test-Service -Name $MockRunningService | Should -Be $true
-        }
-        
-        It "Given the name of a service with a status of 'Stopped', will return false" {
-            Test-Service -Name $MockStoppedService | Should -Be $false
-        }
+    $MockStoppedService = New-MockObject -Type 'System.ServiceProcess.ServiceController' `
+    -Properties @{
+        "ServiceName" = "MockStoppedService";
+        "Status" = "Stopped"
+    }
+    
+    It "Given the name of a service with a status of 'Running', will return true" {
+        Test-Service -Name $MockRunningService | Should -Be $true
+    }
+    
+    It "Given the name of a service with a status of 'Stopped', will return false" {
+        Test-Service -Name $MockStoppedService | Should -Be $false
     }
 }
