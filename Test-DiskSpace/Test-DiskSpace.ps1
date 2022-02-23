6f552a7f59e5bb1bc794406f83dd63730405d28d
JoshuaPiahana <nzsteiny@gmail.com>
2022-02-23 14:24:35 +1300

    passing tests

--- Test-DiskSpace/Test-DiskSpace.ps1
+++ Test-DiskSpace/Test-DiskSpace.ps1
@@ -2,5 +2,14 @@ function Test-DiskSpace {
     param (
         $Disk
     )
-    return
+    
+    $1GB = 1073741824
+
+    if ($Disk.FreeSpace -ge $1GB) {
+        return $true
+    }
+
+    elseif ($Disk.FreeSpace -lt $1GB) {
+        return $false
+    } 
 }
